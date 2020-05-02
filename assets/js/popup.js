e82c22e56be2032d43c77fbe85b7b6b74f92c118
Indrajith C <indranspeaks@gmail.com>
2020-05-02 17:03:44 +0530

    chrome storage used

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,13 +1,31 @@
 $(function () {
   console.log("ready");
-  let total = 0;
   const spend = $("#spend");
   const limit = $("#limit");
   const amount = $("#amount");
 
+  let total = 0;
+  function getInitialValue() {
+    chrome.storage.sync.get(["total"], function (budget) {
+      if (budget.total && !isNaN(budget.total)) {
+        total = budget.total;
+      }
+      spend.val(total);
+    });
+  }
+
   $("#budget-manager").submit(function (event) {
     event.preventDefault();
-    const currentAmount = total + parseFloat(amount.val());
-    spend.val(currentAmount);
+    let total = 0;
+    chrome.storage.sync.get(["total"], function (budget) {
+      if (budget.total && !isNaN(budget.total)) {
+        total = budget.total;
+      }
+      const currentAmount = total + parseFloat(amount.val());
+      spend.val(currentAmount);
+      chrome.storage.sync.set({ total: currentAmount });
+      amount.val("");
+    });
   });
+  getInitialValue();
 });
